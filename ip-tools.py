b755c0753116e4a8360f8123842329576988df32
luke <lukemorton@tutanota.com>
2024-08-28 21:42:27 -0500

    feat: added check for private IPv4

--- ip-tools.py
+++ ip-tools.py
@@ -10,24 +10,60 @@ class IpTools:
             return True
         return False
 
+    def __in_range(self, string, start, end):
+        if not self.isIpv4(string):
+            return False
+
+        # Check against valid byte range
+        def ipBits(ipv4):
+            output = ""
+            for byte in ipv4.split("."):
+                output += f"{int(byte):08b}"
+            return int(output)
+
+        startBits, endBits, stringBits = ipBits(start), ipBits(end), ipBits(string)
+        return stringBits >= startBits and stringBits <= endBits
+
+    def isPrivateIpv4(self, string):
+        return (
+            self.__in_range(string, "10.0.0.0", "10.255.255.255")
+            or self.__in_range(string, "172.16.0.0", "172.31.255.255")
+            or self.__in_range(string, "192.168.0.0", "192.168.255.255")
+        )
+
 
 ipTool = IpTools()
 
 # Test isIpv4
 isIpMap = dict()
 isIpMap["0.0.0.0"] = True
+isIpMap["10.1.75.32"] = True
 isIpMap[""] = False
 isIpMap["123.456.789.000"] = False
 isIpMap["00.00.00.00"] = False
+isIpMap["172.31.35.90"] = True
+isIpMap["172.32.35.90"] = True
 isIpMap["255.255.255.255"] = True
 isIpMap["10.43.89.176"] = True
 isIpMap["apple"] = False
 isIpMap["192.168.1.254.3"] = False
+isIpMap["192.168.1.254"] = True
 
 for entry in isIpMap.keys():
     if ipTool.isIpv4(entry) != isIpMap[entry]:
         print(
-            f"{entry} expected {isIpMap[entry]} but got output {ipTool.isIpv4(entry)}"
+            f"isIpv4({entry}) expected {isIpMap[entry]} but got output {ipTool.isIpv4(entry)}"
+        )
+
+# Test isPrivateIpv4
+isIpMap["0.0.0.0"] = False
+isIpMap["255.255.255.255"] = False
+isIpMap["172.32.35.90"] = False
+
+for entry in isIpMap.keys():
+    if ipTool.isPrivateIpv4(entry) != isIpMap[entry]:
+        print(
+            f"isPrivateIpv4({entry}) expected {isIpMap[entry]} but got output {ipTool.isPrivateIpv4(entry)}"
         )
 
 # Try reading user input for IP addresses
@@ -39,6 +75,9 @@ print(f"{len(inputs)} input(s): {inputs}")
 
 for entry in inputs:
     if ipTool.isIpv4(entry):
-        print(f"{entry} is an IP address.")
+        if ipTool.isPrivateIpv4(entry):
+            print(f"{entry} is a private IPv4 address.")
+        else:
+            print(f"{entry} is a public IPv4 address.")
     else:
         print(f"{entry} is not an IP address.")
